d5122cafb94f4b9c59e88eae366616abc4db526c
welchj <weq511762338@gmail.com>
2026-04-19 21:24:05 -0400

    training for going towards ball done

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -1,6 +1,15 @@
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class Ball : MonoBehaviour
 {
-    
+	private Rigidbody rb;
+
+	private void Awake()
+	{
+		rb = GetComponent<Rigidbody>();
+		rb.interpolation = RigidbodyInterpolation.Interpolate;
+		rb.collisionDetectionMode = CollisionDetectionMode.Continuous;
+		rb.constraints = RigidbodyConstraints.FreezeRotation;
+	}
 }
--- Assets/Scripts/HitBall.cs
+++ Assets/Scripts/HitBall.cs
@@ -3,10 +3,24 @@ using Unity.MLAgents;
 using Unity.MLAgents.Actuators;
 using Unity.MLAgents.Sensors;
 using UnityEngine.InputSystem;
+[RequireComponent(typeof(Rigidbody))]
 public class HitBall : Agent
 {
     private float heuristicSpeed = 3f;
     [SerializeField] private Transform ballTransform;
+    [SerializeField] private float moveSpeed = 3f;
+    private float lastDistanceToBall;
+
+    private Rigidbody rb;
+    private Vector2 moveInput;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.interpolation = RigidbodyInterpolation.Interpolate;
+        rb.collisionDetectionMode = CollisionDetectionMode.ContinuousDynamic;
+        rb.constraints = RigidbodyConstraints.FreezeRotation;
+    }
 
     private void OnTriggerEnter(Collider other)
     {
@@ -22,7 +36,7 @@ public class HitBall : Agent
         // Use collision.gameObject or collision.collider to get the component
         if(collision.gameObject.TryGetComponent<Ball>(out Ball ball))
         {
-            SetReward(1f);
+            SetReward(10f);
             EndEpisode();
         }        
     }
@@ -36,17 +50,48 @@ public class HitBall : Agent
 
     public override void OnActionReceived(ActionBuffers actions)
     {
-        float moveX = actions.ContinuousActions[0];
-        float moveZ = actions.ContinuousActions[1];
+        float moveX = Mathf.Clamp(actions.ContinuousActions[0], -1f, 1f);
+        float moveZ = Mathf.Clamp(actions.ContinuousActions[1], -1f, 1f);
+        moveInput = new Vector2(moveX, moveZ);
 
-        float moveSpeed = 1f;
-        transform.localPosition += new Vector3(moveX, 0, moveZ) * Time.deltaTime * moveSpeed;
+        float distanceToBall = Vector3.Distance(rb.position, ballTransform.position);
+        if (distanceToBall < lastDistanceToBall)
+        {
+            AddReward(0.01f);
+        }
+
+        lastDistanceToBall = distanceToBall;
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 move = new Vector3(moveInput.x, 0f, moveInput.y) * moveSpeed * Time.fixedDeltaTime;
+        rb.MovePosition(rb.position + move);
     }
 
     public override void OnEpisodeBegin()
     {
-        transform.localPosition = new Vector3(Random.Range(-7f, -1f), 1f, Random.Range(-4f, 3f));
-        ballTransform.localPosition = new Vector3(-4f, 1f, 2f);
+        rb.linearVelocity = Vector3.zero;
+        rb.angularVelocity = Vector3.zero;
+        transform.localPosition = new Vector3(Random.Range(-3f, 3f), 1f, Random.Range(-7f, -1f));
+        transform.localRotation = Quaternion.identity;
+
+        Rigidbody ballRb = ballTransform.GetComponent<Rigidbody>();
+        if (ballRb != null)
+        {
+            ballRb.linearVelocity = Vector3.zero;
+            ballRb.angularVelocity = Vector3.zero;
+            ballTransform.localPosition = new Vector3(Random.Range(-3f, 3f), 1f, Random.Range(-7f, -1f));
+            ballTransform.localRotation = Quaternion.identity;
+        }
+        else
+        {
+            ballTransform.localPosition = new Vector3(Random.Range(-3f, 3f), 1f, Random.Range(-7f, -1f));
+            ballTransform.localRotation = Quaternion.identity;
+        }
+
+        moveInput = Vector2.zero;
+        lastDistanceToBall = Vector3.Distance(transform.localPosition, ballTransform.localPosition);
     }
 
 
